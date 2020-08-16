4a886901ab8b27e9328fbb76d109feb6b1686186
cn.yimian.xyz <i@iotcat.me>
2020-08-16 11:18:56 +0800

    add anime

--- checkout.php
+++ checkout.php
@@ -1,3 +1,18 @@
 <?php
+
+include './functions.php';
+
+
+$cnn = db__connect();
+
+if(isset($_COOKIE['_token']) && db__rowNum($cnn, "token", "token", $_COOKIE['_token'])){
+    db__pushData($cnn, "token", array(
+        "state"=>'0'
+    ), array(
+        "token"=>$_COOKIE['_token']
+    ));
+
+}
+
 setcookie("_token", "", time()-3600);
 echo '<script>window.location.href="https://login.yimian.xyz/"</script>';
