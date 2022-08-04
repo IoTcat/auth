ae17484e74a7c85e80c2edbb845993ce93836cee
iotcat <i@iotcat.me>
2022-08-04 14:19:15 +0800

    after mv bugs

--- checkout.php
+++ checkout.php
@@ -15,9 +15,21 @@ if(isset($_COOKIE['_token']) && db__rowNum($cnn, "token", "token", $_COOKIE['_to
         "token"=>$_COOKIE['_token']
     ));
     $redis->hSet('session/dialog/'.$_COOKIE['_token'], 'group', 'anonymous');
+
+//after mv
+}elseif(isset($_COOKIE['_token'])){
+    $token = $_COOKIE['_token'];
+    db__pushData($cnn, "token", array(
+        "hash"=>$token,
+        "token"=>$token,
+        "created_at"=>date("Y-m-d H:i:s", time()),
+        "state"=>'0'
+    ));
+    $redis->hSet('session/dialog/'.$_COOKIE['_token'], 'group', 'anonymous');
 }
 
 if($from)
     echo '<script>window.location.replace("'.base64_decode($from).'")</script>';
 else
     echo '<script>window.location.replace("https://login.yimian.xyz/")</script>';
+
